edb8ad4a7abec129462a7a809cf8ef948d377aae
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2022-01-18 10:43:10 +0100

    Validation for Volume and Channel in smart tv remote

--- src/MenuElements/SmartTv.py
+++ src/MenuElements/SmartTv.py
@@ -46,15 +46,29 @@ def smart_tv_menu(room_t, device_t, controller_t: SmartHomeSystem):
 
         elif is_on and selected_tv_menu == "v":
 
-            new_volume = int(input("Set volume on:\t"))
-            controller_t.use_tv_remote(not is_on, channel, new_volume, is_recording, room_t, device_t)
-            last_data["data"]["volume"] = new_volume
+            while True:
+                new_volume = input("Set volume on (enter \"c\" to cancel):\t")
+                if new_volume == "c":
+                    break
+                new_volume = int(new_volume)
+                if new_volume in range(0, 101):
+                    controller_t.use_tv_remote(not is_on, channel, new_volume, is_recording, room_t, device_t)
+                    last_data["data"]["volume"] = new_volume
+                    break
+                print("Volume must be greater or equal to 0 and less or equal to 100")
 
         elif is_on and selected_tv_menu == "c":
 
-            new_channel = int(input("Enter channel number:\t"))
-            controller_t.use_tv_remote(not is_on, new_channel, volume, is_recording, room_t, device_t)
-            last_data["data"]["channel"] = new_channel
+            while True:
+                new_channel = input("Enter channel number or \"c\" to cancel:\t")
+                if new_channel == "c":
+                    break
+                new_channel = int(new_channel)
+                if new_channel in range(0, 999):
+                    controller_t.use_tv_remote(not is_on, new_channel, volume, is_recording, room_t, device_t)
+                    last_data["data"]["channel"] = new_channel
+                    break
+                print("Channel not found")
 
         elif is_on and selected_tv_menu == "rec":
 
