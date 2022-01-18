b74778108d5105fad08b63719590b5724a959d6d
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2022-01-18 10:31:04 +0100

    Connected smartTV menu with controller

--- src/MenuElements/SmartTv.py
+++ src/MenuElements/SmartTv.py
@@ -13,14 +13,14 @@ def smart_tv_menu(room_t, device_t, controller_t: SmartHomeSystem):
         if is_on:
             is_tv_on = "on"
         if is_recording:
-            is_tv_recording = "off"
+            is_tv_recording = "on"
         print("\nTv is {}".format(is_tv_on))
         tv_menu = {
             "actions": "Available actions:\t",
             "s": "1.\tEnter \"s\" to switch tv power",
             "r": "2.\tEnter \"r\" to return"
         }
-        if is_tv_on:
+        if is_on:
             print("Channel: {}, Volume: {}/100, Recording is {}".format(channel, volume, is_tv_recording))
             tv_menu = {
                 "actions": "Available actions:\t",
@@ -33,15 +33,33 @@ def smart_tv_menu(room_t, device_t, controller_t: SmartHomeSystem):
         for tv_menu_option in tv_menu.values():
             print(tv_menu_option)
         selected_tv_menu = input(f'{room_t}/{device_t}#\t')
+
+        # menu actions
         if selected_tv_menu == "r":
+
             break
+
         elif selected_tv_menu == "s":
-            pass
-        elif selected_tv_menu == "v":
-            pass
-        elif selected_tv_menu == "c":
-            pass
-        elif selected_tv_menu == "rec":
-            pass
+
+            controller_t.use_tv_remote(not is_on, channel, volume, is_recording, room_t, device_t)
+            last_data["data"]["is_on"] = not last_data["data"]["is_on"]
+
+        elif is_on and selected_tv_menu == "v":
+
+            new_volume = int(input("Set volume on:\t"))
+            controller_t.use_tv_remote(not is_on, channel, new_volume, is_recording, room_t, device_t)
+            last_data["data"]["volume"] = new_volume
+
+        elif is_on and selected_tv_menu == "c":
+
+            new_channel = int(input("Enter channel number:\t"))
+            controller_t.use_tv_remote(not is_on, new_channel, volume, is_recording, room_t, device_t)
+            last_data["data"]["channel"] = new_channel
+
+        elif is_on and selected_tv_menu == "rec":
+
+            controller_t.use_tv_remote(is_on, channel, volume, not is_recording, room_t, device_t)
+            last_data["data"]["is_recording"] = not last_data["data"]["is_recording"]
+
         else:
             print("Invalid option")
