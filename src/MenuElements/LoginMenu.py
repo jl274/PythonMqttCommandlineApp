f9e401ded28aa129ff6080403c5dd7e3c33dc031
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2022-01-20 09:20:10 +0100

    Login menu

---
+++ src/MenuElements/LoginMenu.py
@@ -0,0 +1,18 @@
+import hashlib
+import requests
+import getpass
+
+
+def LoginMenu() -> str:
+    """
+    :return: "root" or "user
+    """
+    while True:
+        login = input("Login:\t")
+        # password = getpass.getpass(prompt='Password:\t', stream=None)
+        password = input("Password:\t")     # temp for testing purposes
+        hashed_password = hashlib.md5(password.encode()).hexdigest()
+
+        r = requests.get(f"http://localhost:5000/login/{login}", json={"password": hashed_password}).json()
+        if r["logged"]:
+            return r["role"]
